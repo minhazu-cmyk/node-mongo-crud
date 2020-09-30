0a0bc37bf5d27eb9b7cdff2dd8f1b0ef873053bd
minhazu-cmyk <minhajabedin341@gmail.com>
2020-09-30 10:41:36 +0000

    first commit

--- index.js
+++ index.js
@@ -31,19 +31,38 @@ client.connect(err => {
       res.send(documents)
     })
   })
+  app.get ("/product/:id", (req,res)=>{
+    productCollection.find({_id: ObjectId(req.params.id)})
+    .toArray((err,documents)=>{
+      res.send(documents[0]);
+    })
+  })
 
   app.post("/addProduct", (req,res)=>{
    const product = req.body
   productCollection.insertOne(product)
   .then(result=>{
     console.log("data added succesfully")
-    res.send('success')
+    res.redirect('/')
   })
   })
+  app.patch('/update/:id',(req,res)=>{
+    productCollection.updateOne({_id: ObjectId(req.params.id)},
+    {
+      $set: {price: req.body.price, quantity: req.body.quantity}
+    }
+    )
+    .then(result=>{
+      
+      res.send(result.modifiedCount > 0);
+    })
+  })
+
+
 app.delete ('/delete/:id', (req,res)=>{
  productCollection.deleteOne({_id: ObjectId(req.params.id)})
  .then(result=>{
-  console.log(result);
+  res.send(result.deletedCount>0)
  })
 })
 
